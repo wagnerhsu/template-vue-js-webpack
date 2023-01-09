effa6fc19d5390fa7e940b01272bacfe45df672f
Wagner Xu <wagnerhsu@hotmail.com>
2023-01-09 22:21:55 +0800

    Button, DatePicker, message

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
-createApp(App).use(router).mount('#app');
+import { Button, DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+const app = createApp(App);
+app.use(Button).use(DatePicker);
+app.use(router).mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+  <a-button type="primary" @click="info">Primary Button</a-button>
+  <a-date-picker v-model:value="value1" />
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
+import { message } from 'ant-design-vue';
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+const value1 = ref();
+const info = () => {
+  message.info('This is a normal message');
 };
 </script>
