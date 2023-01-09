f5a8bcd7f50de0ae538b10cc504777b83001bd1e
Wagner Xu <wagnerhsu@hotmail.com>
2023-01-09 22:28:09 +0800

    feat: use Button rather than a-button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,16 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+  <Button type="primary">
+    Primary Button using Button rather than a-button
+  </Button>
   <a-button type="primary" @click="info">Primary Button</a-button>
   <a-date-picker v-model:value="value1" />
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { message } from 'ant-design-vue';
+import { Button, message } from 'ant-design-vue';
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 const value1 = ref();
